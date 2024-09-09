0d34774152850a0b42abca7be7a7e6f896d1f605
Alan <alan@fullworks.net>
2024-09-09 21:54:57 +0100

    Refactor plugin initialization and settings handling
    Converted plugin shortname to static and added plugin file and settings page properties. Introduced activation, uninstallation hooks, and new settings link method. Enhanced conditional checks for admin screen redirections and adjusted option handling to use site-wide options.

--- src/Main.php
+++ src/Main.php
@@ -6,7 +6,7 @@ class Main {
 	/**
 	 * @var mixed
 	 */
-	private $plugin_shortname;
+	private static $plugin_shortname;
 	/**
 	 * @var mixed
 	 */
@@ -15,34 +15,67 @@ class Main {
 	 * @var \WP_Screen|null
 	 */
 	private $current_page;
+	/**
+	 * @var mixed
+	 */
+	private $plugin_file;
+	/**
+	 * @var mixed
+	 */
+	private $settings_page;
 
-	public function __construct( $plugin_shortname, $page ) {
-		$this->plugin_shortname = $plugin_shortname;
+	public function __construct( $plugin_file, $settings_page, $plugin_shortname, $page ) {
+		self::$plugin_shortname = $plugin_shortname;
 		$this->page             = $page;
-
+		$this->plugin_file      = $plugin_file;
+		$this->settings_page    = $settings_page;
+		register_activation_hook( $this->plugin_file, array( $this, 'plugin_activate' ) );
+		register_uninstall_hook( $this->plugin_file, array( '\Fullworks_Free_Plugin_Lib\Main', 'plugin_uninstall' ) );
+		add_filter( 'plugin_action_links_' . $this->plugin_file, array( $this, 'settings_link' ) );
 		add_action( 'init', array( $this, 'load_text_domain' ) );
-
-		$this->current_page = $screen = get_current_screen();
-
 		add_action( 'admin_menu', array( $this, 'add_settings_page' ) );
-
-		if ( is_admin() && $this->current_page->id === 'settings_page_ffpl-opt-in' ) {
-			add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_assets' ) );
-		}
-
-		if ( is_admin() && $this->current_page->id === $this->page ) {
-			if ( ! get_option( $this->plugin_shortname . '_form_rendered' ) ) {
-				wp_safe_redirect( admin_url( 'admin.php?options-general.php?page=ffpl-opt-in' ) );
+		add_action( 'current_screen', function () {
+			$this->current_page = get_current_screen();
+			if ( is_admin() && $this->current_page->id === 'settings_page_ffpl-opt-in' ) {
+				add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_assets' ) );
 			}
 
+			if ( is_admin() && $this->current_page->id === $this->page ) {
+				if ( ! get_site_option( self::$plugin_shortname . '_form_rendered' ) ) {
+					wp_safe_redirect( admin_url( 'options-general.php?page=ffpl-opt-in' ) );
+				}
+			}
+		} );
+	}
+
+	public function plugin_activate() {
+		if ( ! get_site_option( self::$plugin_shortname . '_form_rendered' ) ) {
+			if ( ! isset( $_GET['activate-multi'] ) ) {
+				wp_safe_redirect( admin_url( $this->settings_page ) );
+			}
 		}
 	}
 
+	public static function plugin_uninstall() {
+		delete_site_option( self::$plugin_shortname . '_form_rendered' );
+	}
+
+	public function settings_link( $links ) {
+		$settings_link = '<a href="' . esc_url( $this->settings_page ) . '">' . esc_html( $this->translate( 'Settings' ) ) . '</a>';
+		array_unshift(
+			$links,
+			$settings_link
+		);
+
+		return $links;
+	}
+
+
 	public function add_settings_page() {
-		if ( ! get_option( $this->plugin_shortname . '_form_rendered' ) ) {
+		if ( ! get_site_option( self::$plugin_shortname . '_form_rendered' ) ) {
 			add_options_page(
-				$this->esc_html__( 'Opt In', 'free-plugin-lib' ), // Page title
-				$this->esc_html__( 'Opt In', 'free-plugin-lib' ), // Menu title
+				esc_html( $this->translate( 'Opt In' ) ), // Page title
+				esc_html( $this->translate( 'Opt In' ) ), // Menu title
 				'manage_options', // Capability
 				'ffpl-opt-in', // Menu slug
 				array( $this, 'render_opt_in_page' ) // Callback function
@@ -71,7 +104,9 @@ class Main {
 	}
 
 
-	private function esc_html__( $text, $domain ) {
-		esc_html( translate( $text, $domain ) );
+	private function translate( $text ) {
+		// deliberately done like this to stop polygots auto adding to translation files as
+		// wp.org doesn't differentiate text domains
+		return translate( $text, 'free-plugin-lib' );
 	}
 }
