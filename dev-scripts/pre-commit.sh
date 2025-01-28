3dd9c62b4c858e9c36d88c84b8deb6872461d363
Alan <alan@fullworks.net>
2025-01-28 13:45:26 +0000

    add workflow

--- dev-scripts/pre-commit.sh
+++ dev-scripts/pre-commit.sh
@@ -11,3 +11,13 @@ fi
 
 echo "Version sync successful. Proceeding with commit."
 exit 0
+
+./vendor/bin/phpunit   tests
+
+if [ $? -ne 0 ]; then
+  echo "Error: Tests Failed. Aborting commit."
+  exit 1
+fi
+
+echo "Tests Suceeded. Proceeding with commit."
+exit 0
